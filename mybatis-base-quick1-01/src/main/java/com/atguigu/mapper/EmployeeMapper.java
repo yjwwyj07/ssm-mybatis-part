055d55ebf217b6c1a94946df034ce71829e16f6e
yjwwyj07 <yjwang0722@gmail.com>
2024-01-26 16:48:10 -0500

    Mybatis Input Output

--- mybatis-base-quick1-01/src/main/java/com/atguigu/mapper/EmployeeMapper.java
+++ mybatis-base-quick1-01/src/main/java/com/atguigu/mapper/EmployeeMapper.java
@@ -1,6 +1,10 @@
 package com.atguigu.mapper;
 
 import com.atguigu.pojo.Employee;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * t_emp表对应数据库SQL语句映射接口!
@@ -14,6 +18,28 @@ public interface EmployeeMapper {
      * @param empId  员工id
      * @return 员工实体对象
      */
+    //数据输入
     Employee selectEmployee(Integer empId);
     Employee queryById(Integer empId);
+    int deleteById(Integer id);
+    List<Employee> queryBySalary(Double salary);
+
+    int insertEmp(Employee employee);
+    List<Employee> queryByNameAndSalary(@Param("name") String name,@Param("salary") String salary);
+
+    int insertEmpMap(Map data);
+    //数据输出
+    //删除插入修改 返回值int
+    int deleteByEmpId(Integer id);
+    //查询
+    String queryNameById(Integer id);
+
+    Double querySalaryById(Integer id);
+
+    Map<String,Object> selectEmpNameAndMaxSalary();
+
+    List<String> queryNameBySalary(Double salary);
+
+    List<Employee> queryAll();
+
 }
--- mybatis-base-quick1-01/src/test/java/MyBatisTest.java
+++ mybatis-base-quick1-01/src/test/java/MyBatisTest.java
@@ -1,6 +1,8 @@
 import com.atguigu.mapper.EmployeeMapper;
+import com.atguigu.mapper.TeacherMapper;
 import com.atguigu.pojo.Employee;
 import com.atguigu.pojo.Student;
+import com.atguigu.pojo.Teacher;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 /**
  * projectName: com.atguigu.test
@@ -93,6 +96,70 @@ public class MyBatisTest {
         Employee employee1 = employeeMapper.queryById(1);
         System.out.println("employee = " + employee);
         System.out.println("employee1 = " + employee1);
+
+        // 4.关闭SqlSession
+        session.commit(); //提交事务 [DQL不需要,其他需要]
+        session.close(); //关闭会话
+
+    }
+    @Test
+    public void test_04() throws IOException {
+
+        // 1.创建SqlSessionFactory对象
+        // ①声明Mybatis全局配置文件的路径
+        String mybatisConfigFilePath = "mybatis-config.xml";
+
+        // ②以输入流的形式加载Mybatis配置文件
+        InputStream inputStream = Resources.getResourceAsStream(mybatisConfigFilePath);
+
+        // ③基于读取Mybatis配置文件的输入流创建SqlSessionFactory对象
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2.使用SqlSessionFactory对象开启一个会话
+        SqlSession session = sessionFactory.openSession();
+
+        // 3.根据EmployeeMapper接口的Class对象获取Mapper接口类型的对象(动态代理技术)
+        EmployeeMapper employeeMapper = session.getMapper(EmployeeMapper.class);
+
+        // 4. 调用代理类方法既可以触发对应的SQL语句
+        Employee employee = new Employee();
+        employee.setEmpName("James");
+        employee.setEmpSalary(5654.1);
+        int rows = employeeMapper.insertEmp(employee);
+        System.out.println("rows = " + rows);
+        System.out.println(employee.getEmpId());
+        // 4.关闭SqlSession
+        session.commit(); //提交事务 [DQL不需要,其他需要]
+        session.close(); //关闭会话
+
+    }
+    @Test
+    public void test_05() throws IOException {
+
+        // 1.创建SqlSessionFactory对象
+        // ①声明Mybatis全局配置文件的路径
+        String mybatisConfigFilePath = "mybatis-config.xml";
+
+        // ②以输入流的形式加载Mybatis配置文件
+        InputStream inputStream = Resources.getResourceAsStream(mybatisConfigFilePath);
+
+        // ③基于读取Mybatis配置文件的输入流创建SqlSessionFactory对象
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2.使用SqlSessionFactory对象开启一个会话
+        SqlSession session = sessionFactory.openSession();
+
+        // 3.根据EmployeeMapper接口的Class对象获取Mapper接口类型的对象(动态代理技术)
+        TeacherMapper mapper = session.getMapper(TeacherMapper.class);
+
+
+        Teacher teacher = new Teacher();
+        teacher.settName("Li");
+        System.out.println(teacher.gettId());
+        int i = mapper.insertTeacher(teacher);
+        System.out.println("i = " + i);
+        System.out.println(teacher.gettId());
+        // 4. 调用代理类方法既可以触发对应的SQL语句
 
         // 4.关闭SqlSession
         session.commit(); //提交事务 [DQL不需要,其他需要]
